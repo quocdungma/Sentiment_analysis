846f7ea462a2a58073da112ee4260511bb006af4
quocdungma <144505247+quocdungma@users.noreply.github.com>
2023-09-15 17:06:34 +0700

    Update app.py

--- app.py
+++ app.py
@@ -272,9 +272,9 @@ sidebar_option = st.sidebar.selectbox(
     ("Giới thiệu", "🔍 Tìm kiếm theo sản phẩm", "Nhập bình luận để dự đoán")
 )
 
-st.sidebar.markdown(f"<h1 style='font-size:20px;'>😊 Tích cực</h1>", unsafe_allow_html=True)
-st.sidebar.markdown(f"<h1 style='font-size:20px;'>😐 Trung tính</h1>", unsafe_allow_html=True)
-st.sidebar.markdown(f"<h1 style='font-size:20px;'>😢 Tiêu cực</h1>", unsafe_allow_html=True)
+st.sidebar.markdown(f"<h1 style='font-size:10px;'>😊 Tích cực</h1>", unsafe_allow_html=True)
+st.sidebar.markdown(f"<h1 style='font-size:10px;'>😐 Trung tính</h1>", unsafe_allow_html=True)
+st.sidebar.markdown(f"<h1 style='font-size:10px;'>😢 Tiêu cực</h1>", unsafe_allow_html=True)
 
 if sidebar_option == "Giới thiệu":
     st.title("Giới thiệu về ứng dụng")
